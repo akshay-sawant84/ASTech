14fb3fa661a73fc01fb81aa52df982e8998479c5
Akshay <akshaysawant8450@gmail.com>
2020-05-23 22:28:46 +0530

    added preloader and animated form

--- rocket.js
+++ rocket.js
@@ -42,3 +42,52 @@
     });
 
 })(jQuery);
+
+
+//parallex
+
+(function($) {
+    /** change value here to adjust parallax level */
+    var parallax = -0.5;
+
+    var $bg_images = $(".wp-block-cover-image");
+    var offset_tops = [];
+    $bg_images.each(function(i, el) {
+        offset_tops.push($(el).offset().top);
+    });
+
+    $(window).scroll(function() {
+        var dy = $(this).scrollTop();
+        $bg_images.each(function(i, el) {
+            var ot = offset_tops[i];
+            $(el).css("background-position", "50% " + (dy - ot) * parallax + "px");
+        });
+    });
+})(jQuery);
+
+//cursor pointer
+
+
+
+//mail
+
+$('.form__btn').click(function() {
+    $('.mail__letter').toggleClass('move');
+    $('.mail__top').toggleClass('closed');
+    $('.form__btn--invisible').toggleClass('visible');
+    $('.form__btn--visible').toggleClass('invisible');
+});
+
+$('input').focus(function() {
+    $(this).parent().addClass('active');
+    $('input').focusout(function() {
+        if ($(this).val() == '') {
+            $(this).parent().removeClass('active');
+        } else {
+            $(this).parent().addClass('active');
+        }
+    })
+});
+
+
+//paper plane animation
